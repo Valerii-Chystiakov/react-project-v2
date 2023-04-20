52421e57e1f7f31e1d79495a25db8a05420beaf4
Valerii Chystiakov <hnu.kn20.2.chistyakov@gmail.com>
2023-04-20 15:44:05 +0300

    state-and-change-state-name-onClick

--- src/ClassComponent.js
+++ src/ClassComponent.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react'
 
 export default class ClassComponent extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+      name: "Alex"
+    }
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    this.setState({
+      name: "Hiiiiiiiiiiiiiiiiiiiiiii"
+    })
+  }
   render() {
-    return (
+        return (
       <div>ClassComponent
-        <h2>{this.props.numbers.join(" ")}</h2>
+        <h2>{this.state.name}</h2>
+        <button onClick={this.handleClick}>Click</button>
       </div>
     )
   }
